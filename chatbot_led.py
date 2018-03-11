866356ce5aac9df9ade6d3aee48e640142de8c0d
mitya8128 <dmitriy.akhmediyev@gmail.com>
2018-03-11 12:13:56 +0000

    connected to arduino

--- chatbot_led.py
+++ chatbot_led.py
@@ -9,11 +9,22 @@ import os
 import pygame
 from pygame.locals import *
 import speech_recognition as sr
+import serial
 
-chatbot_2 = ChatBot('holo_chatbot2', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
 
-chatbot_2.train('chatterbot.corpus.english')
+chatbot_test = ChatBot('holo_chatbot', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
 
+#os.chdir(r'C:\\Users\\Admin\Desktop\\chatbot_test')
+
+#THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+#my_file = os.path.join(THIS_FOLDER, 'wittgenstein.yml')
+
+#chatbot_test.train(my_file)
+
+chatbot_test.train('chatterbot.corpus.english.conversations')
+
+#объявляем устройство
+ser = serial.Serial('/dev/tty.usbserial', 9600)
 
 #функция, создающая уникальное имя файла
 import string
@@ -22,6 +33,13 @@ import random
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+
+alphabet_colours = ('r','g','b','y','c','m')
+
+def str_gen(alphabet_colours):
+    for i in range(9):
+        return random.choice(alphabet_colours)
+        
 #функция которая убивает процесс
 def kill_by_process_name_shell(name):
     os.system("taskkill /f /im " + name)
@@ -37,14 +55,15 @@ def play_movie(path):
 #получить ответ чатбота,напечатав вопрос
 def get_chatbot_response(text):
     text = input()
-    response = chatbot_2.get_response(text)
+    response = chatbot_test.get_response(text)
     response_str = str(response)
     tts = gTTS(text=response_str, lang='en-us', slow=False)
     file_name = str(id_generator(6))                   #добавить неповторяющееся имя файла  
     file = tts.save('%s.mp3' % file_name)
     print(response_str)
     pygame.mixer.init()
-    pygame.mixer.music.load('%s.mp3' % file_name)     
+    #pygame.mixer.music.load('%s.mp3' % file_name)     
+    pygame.mixer.music.load('%s.mp3' % file_name)
     pygame.mixer.music.play()
     #play_movie('conversation.mp4')
 
@@ -66,7 +85,7 @@ def get_chatbot_response2(rec_text):
             m = 'My speech recognition service has failed. {0}'
             return Statement(m.format(e))
     '''
-    response = chatbot_2.get_response(rec_text)
+    response = chatbot_test.get_response(rec_text)
     response_str = str(response)
     tts = gTTS(text=response_str, lang='en-us', slow=False)
     file_name = str(id_generator(6))                   #добавить неповторяющееся имя файла  
@@ -80,7 +99,7 @@ def get_chatbot_response2(rec_text):
 #функция проверяющая наличие подключения к интернету
 import requests
 
-def connected_to_internet(url='http://www.google.com/', timeout=10):
+def connected_to_internet(url='http://www.google.com/', timeout=5):
     try:
         _ = requests.get(url, timeout=timeout)
         return True
@@ -107,3 +126,11 @@ def v():
         pygame.mixer.init()
         pygame.mixer.music.load("Tell me about your self.mp3")     
         pygame.mixer.music.play()
+        
+        
+while True:
+    try:
+        get_response()
+        ser.write(str_gen(alphabet_colours))        #maybe ser.write(b'response_str')
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
