1f3ccce67eaf8042cb3707d31bfd1d1c9f3a4b6e
mitya <dmitriy.akhmediyev@gmail.com>
2020-06-23 01:14:36 +0300

    comments, pep8 reformatting

--- chatbot.py
+++ chatbot.py
@@ -1,55 +1,53 @@
-
-# coding: utf-8
-
-
-
 from chatterbot import ChatBot
 from gtts import gTTS
 import os
 import pygame
 from pygame.locals import *
 import speech_recognition as sr
+import string
+import random
+import time
+import requests
+from os import startfile
 
 chatbot_2 = ChatBot('holo_chatbot2', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
 
 chatbot_2.train('chatterbot.corpus.english')
 
 
-#функция, создающая уникальное имя файла
-import string
-import random
-
+# unique filename
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-#функция которая убивает процесс
+
+# kill process
 def kill_by_process_name_shell(name):
     os.system("taskkill /f /im " + name)
 
-import time
 
 def play_movie(path):
-    from os import startfile
     startfile(path)
     time.sleep(10)
     kill_by_process_name_shell('mplayerc.exe')
 
-#получить ответ чатбота,напечатав вопрос
+
+# get response
 def get_chatbot_response(text):
     text = input()
     response = chatbot_2.get_response(text)
     response_str = str(response)
     tts = gTTS(text=response_str, lang='en-us', slow=False)
-    file_name = str(id_generator(6))                   #добавить неповторяющееся имя файла  
+    file_name = str(id_generator(6))  # unique filename
     file = tts.save('%s.mp3' % file_name)
     print(response_str)
     pygame.mixer.init()
-    #pygame.mixer.music.load('%s.mp3' % file_name)     
+    # pygame.mixer.music.load('%s.mp3' % file_name)
     pygame.mixer.music.load('%s.mp3' % file_name)
     pygame.mixer.music.play()
-    #play_movie('conversation.mp4')
+    # play_movie('conversation.mp4')
 
-#получить ответ чатбота, задав вопрос голосом      
+
+# get response through voice recognition
 def get_chatbot_response2(rec_text):
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -59,7 +57,7 @@ def get_chatbot_response2(rec_text):
     print(rec_text)
     '''
      try:
-            result = recognizer_function(audio)                               #TODO:небольшой обработчик ошибок для распознавания речи
+            result = recognizer_function(audio)                               #TODO:exceptions worker
             return Statement(result)
         except speech_recognition.UnknownValueError:
             return Statement('I am sorry, I could not understand that.')
@@ -70,16 +68,16 @@ def get_chatbot_response2(rec_text):
     response = chatbot_2.get_response(rec_text)
     response_str = str(response)
     tts = gTTS(text=response_str, lang='en-us', slow=False)
-    file_name = str(id_generator(6))                   #добавить неповторяющееся имя файла  
-    file = tts.save('%s.mp3' % file_name)             
+    file_name = str(id_generator(6))
+    file = tts.save('%s.mp3' % file_name)
     print(response_str)
     pygame.mixer.init()
-    pygame.mixer.music.load('%s.mp3' % file_name)     
+    pygame.mixer.music.load('%s.mp3' % file_name)
     pygame.mixer.music.play()
-    #play_movie('conversation.mp4')
-    
-#функция проверяющая наличие подключения к интернету
-import requests
+    # play_movie('conversation.mp4')
+
+
+# check internet connection
 
 def connected_to_internet(url='http://www.google.com/', timeout=5):
     try:
@@ -89,27 +87,29 @@ def connected_to_internet(url='http://www.google.com/', timeout=5):
         print("No internet connection available.")
     return False
 
-#нажатие клавиши пользователя в питоновской консоли
+
+# typing scenario
 def get_response():
     if connected_to_internet() is True:
         get_chatbot_response('enter')
-        #get_chatbot_response2('rec_text')
+        # get_chatbot_response2('rec_text')
     if connected_to_internet() is False:
         pygame.mixer.init()
-        pygame.mixer.music.load("Tell me about your self.mp3")     
+        pygame.mixer.music.load("Tell me about your self.mp3")
         pygame.mixer.music.play()
 
-#запустить голосовое распознавание вопроса
+
+# voice recognition scenario
 def v():
     if connected_to_internet() is True:
-        #get_chatbot_response('enter')
+        # get_chatbot_response('enter')
         get_chatbot_response2('rec_text')
     if connected_to_internet() is False:
         pygame.mixer.init()
-        pygame.mixer.music.load("Tell me about your self.mp3")     
+        pygame.mixer.music.load("Tell me about your self.mp3")
         pygame.mixer.music.play()
-        
-        
+
+
 while True:
     try:
         get_response()
